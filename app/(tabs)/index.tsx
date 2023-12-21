c466943ba9c6b481bbfd4c4e041eb755bb46fda6
ᴸᴿKyle <lr.zkkat@gmail.com>
2023-12-20 20:45:54 -0500

    .

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,13 +4,13 @@ import { StyleSheet } from 'react-native';
 import EditScreenInfo from '../../components/EditScreenInfo';
 import { Text, View } from '../../components/Themed';
 
-
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Language Learning </Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)"/>
+      <TextInput style ={styles.input} placeholder = "Add"/>
+      <EditScreenInfo path="app/(tabs)/index.tsx"/>
     </View>
   );
 }
@@ -21,7 +21,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  
+
   title: {
     fontSize: 20,
     fontWeight: 'bold',
