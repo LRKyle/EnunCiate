c82476a1d5d8c931388c3668dc13bad5d81e153f
ᴸᴿKyle <lr.zkkat@gmail.com>
2023-12-25 10:45:02 -0500

    UI Kitten Confusion

--- App.js
+++ App.js
@@ -1,23 +1,16 @@
 import React from 'react'
 import * as eva from '@eva-design/eva'
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text} from 'react-native';
-import {ApplicationProvider, Input, Layout, Select, SelectItem, IndexPath} from '@ui-kitten/components'
+import { StyleSheet} from 'react-native'
+import {ApplicationProvider, Input, Layout, Text, Select, SelectItem, IndexPath} from '@ui-kitten/components'
 
 export default function App() {
   const [value, setValue] = React.useState('');
-  const [selectedIndex, setSelectedIndex] = React.useState<IndexPath | IndexPath[0]>(new IndexPath(0));
-//<StatusBar style="auto" />
   return (
     <ApplicationProvider {...eva} theme = {eva.dark}>
+
+      <Text>{value}</Text>
       <Layout style={styles.container}>
         <Input style ={styles.input} placeholder = 'Enter a word' value={value} onChangeText={nextValue => setValue(nextValue)}/>
-        <Text>{value}</Text>
-        <Select selectedIndex={selectedIndex} onSelect={index => setSelectedIndex(index)}>
-          <SelectItem title='Option 1'/>
-          <SelectItem title='Option 2'/>
-          <SelectItem title='Option 3'/>
-        </Select>
       </Layout>
     </ApplicationProvider>
   );
@@ -30,6 +23,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  text:{
+    color: "red",
   },
 
   input: {
