2e30d4ec729acbad9c4188881d3d7c84436fa6ab
ᴸᴿKyle <lr.zkkat@gmail.com>
2023-12-26 03:12:54 -0500

    Altering the button color

--- App.js
+++ App.js
@@ -37,7 +37,7 @@ export default function App() {
           >{data.map((item, index) => (<SelectItem key={index} title={item.text}/>))}
           </Select>
         </Layout>
-        <Button style = {{margin: 2}} appearance='outline'>Analyze your voice!</Button> 
+        <Button status='warning' appearance='outline'>Analyze your voice!</Button> 
       </Layout>
     </ApplicationProvider>
   );
@@ -55,7 +55,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center'
   },
-
+  
   input: {
     fontSize: 15,
     fontStyle: 'italic',
