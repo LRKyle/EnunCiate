5907dc7161a6355f856e645cc979abe67ade80cf
ᴸᴿKyle <lr.zkkat@gmail.com>
2023-12-31 23:43:05 -0500

    Added audio recording

--- App.js
+++ App.js
@@ -8,7 +8,7 @@ const Stack = createNativeStackNavigator();
 export default function App() {  
   return(
     <NavigationContainer> 
-      <Stack.Navigator initialRouteName='Search'>
+      <Stack.Navigator initialRouteName='Analyze'>
         <Stack.Screen name="Search" component={Search}/>
         <Stack.Screen name="Analyze" component={Analyze}  initialParams={{searchVal: "Placeholder", langVal: "Placeholder"}}/>
       </Stack.Navigator>
--- screens/Analyze.js
+++ screens/Analyze.js
@@ -1,22 +1,59 @@
 import React, {useState} from 'react'
 import * as eva from '@eva-design/eva'
-import {StyleSheet} from 'react-native'
-import {ApplicationProvider, Layout, Text, Select, SelectItem, Divider, Button} from '@ui-kitten/components'
-import { Recording } from 'expo-av/build/Audio'
+import {StyleSheet, Button} from 'react-native'
+import {ApplicationProvider, Layout, Text, Select, SelectItem, Divider} from '@ui-kitten/components'
 import {Audio} from 'expo-av'
+import * as sdk from 'microsoft-cognitiveservices-speech-sdk';
 
+let recording = new Audio.Recording();
 
 export const Analyze = ({route}) => {
-    const {searchVal, langVal} = route.params
+  const {searchVal, langVal} = route.params
+  const [recording, setRecording] = React.useState();
 
-    return (
-        <ApplicationProvider {...eva} theme = {eva.dark}>
-            <Layout style= {styles.container}>
-                <Button titlestatus='warning' appearance='outline' onPress={rec ? stopRecording : startRecording}>{rec ? 'Stop Recording' : 'Start Recording'}</Button>
-                <Button onPress={clearRec}></Button>
-            </Layout>
-        </ApplicationProvider>
+  async function startRecording() {
+    try {
+      console.log('Requesting permissions..');
+      await Audio.requestPermissionsAsync();
+      await Audio.setAudioModeAsync({
+        allowsRecordingIOS: true,
+        playsInSilentModeIOS: true,
+      });
+
+      console.log('Starting recording..');
+      const { recording } = await Audio.Recording.createAsync( Audio.RecordingOptionsPresets.HIGH_QUALITY
+      );
+      setRecording(recording);
+      console.log('Recording started');
+    } catch (err) {
+      console.error('Failed to start recording', err);
+    }
+  }
+
+  async function stopRecording() {
+    console.log('Stopping recording..');
+    setRecording(undefined);
+    await recording.stopAndUnloadAsync();
+    await Audio.setAudioModeAsync(
+      {
+        allowsRecordingIOS: false,
+      }
     );
+    const uri = recording.getURI();
+    console.log('Recording stopped and stored at', uri);
+  }
+
+  
+  return (
+      <ApplicationProvider {...eva} theme = {eva.dark}>
+          <Layout style= {styles.container}>
+            <Button
+              title={recording ? 'Stop Recording' : 'Start Recording'}
+              onPress={recording ? stopRecording : startRecording}
+            />
+          </Layout>
+      </ApplicationProvider>
+  );
 }
 
 const styles = StyleSheet.create({
@@ -26,3 +63,6 @@ const styles = StyleSheet.create({
       justifyContent: 'center',
     }
 });
+
+
+//var pronunciationAssessmentConfig = SpeechSDK.PronunciationAssessmentConfig.fromJSON("{\"referenceText\":\"good morning\",\"gradingSystem\":\"HundredMark\",\"granularity\":\"Phoneme\",\"phonemeAlphabet\":\"IPA\"}");
