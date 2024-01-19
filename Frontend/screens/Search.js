9ebad5a8dda42eb81164fd155ff17cef10dc595c
ᴸᴿKyle <lr.zkkat@gmail.com>
2024-01-19 13:55:51 -0500

    Added loading screen
    Aswell as a way to bring you to the main screen, if you don't have a connection or the request didn't work

--- Frontend/screens/Search.js
+++ Frontend/screens/Search.js
@@ -1,11 +1,22 @@
 import React, {useState} from 'react'
 import * as eva from '@eva-design/eva'
-import {TouchableWithoutFeedback, StyleSheet} from 'react-native'
+import {TouchableWithoutFeedback, StyleSheet, ActivityIndicator} from 'react-native'
 import {Audio} from 'expo-av'
-import {ApplicationProvider, Input, Layout, Text, Select, SelectItem, SelectGroup, Divider, Button, Icon, IconRegistry,} from '@ui-kitten/components'
+import {ApplicationProvider, Input, Layout, Text, Select, SelectItem, SelectGroup, Divider, Button, Icon, IconRegistry, Spinner, Popover} from '@ui-kitten/components'
 import { EvaIconsPack } from '@ui-kitten/eva-icons'
 import ky from 'ky'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
+const langSetting = [
+  {
+    language: 'English',
+    dialects: ['American Dialect', 'Canadian Dialect', 'British Dialect', 'Australia Dialect', 'Indian Dialect'],
+    regionCode: ['en-US', 'en-CA', 'en-GB', 'en-AU', 'en-IN'] 
+  },
+  {language: 'French', dialects: ['French Dialect', 'Canadian Dialect'], regionCode: ['fr-FR', 'fr-CA']},
+  {language: 'Spanish', dialects: ['Spaniard Dialect', 'Mexico Dialect'], regionCode: ['es-ES', 'es-MX']},
+  {language: 'Chinese', dialects: ['Mandarin Dialect (Simplified)', 'Cantonese Dialect (Traditional)'], regionCode: ['zh-CN', 'zh-HK']},
+];
 
 
 export const Search = ({navigation}) => {
@@ -16,6 +27,9 @@ export const Search = ({navigation}) => {
   const [uri, setURI] = useState('');
   const [selectedValue, setSelectedValue] = useState('');
   const [lang, setLang] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [visible, setVisible] = React.useState(false);
+
   
   //micOff / micOn  
   const micOutline = (props) => (<TouchableWithoutFeedback onPress={startRecording}><Icon {...props} fill = {'#8F9BB3'} name='mic-outline'/></TouchableWithoutFeedback>);
@@ -54,36 +68,27 @@ export const Search = ({navigation}) => {
     console.log('Recording stopped and stored');
   }
 
-  const test = async () => {
+  const submit = async () => {
+    setIsLoading(true)
     const filetype = uri.split(".").pop();
     const fd = new FormData();
     fd.append("audio-record", {
-      name: "audioFilessssssss",
+      name: "audioFile",
       uri: uri,
       type: `audio/${filetype}`,
     });
     fd.append("searchVal", value);
     fd.append("lang", lang);
-
-    await ky.post(process.env.REACT_APP_AUDIO, {
-      body: fd
-    });
-
-    navigation.navigate('Analyze', {searchVal: value, lang: lang});
-    console.log("Life or death")
-    //Add a loading screen - https://www.youtube.com/watch?v=mZXCOdIFg6Q&pp=ygUrU2V0dXAgYSBsb2FkaW5nIHBhZ2UgUmVhY3QgTmF0aXZlIFVJIEtpdHRlbg%3D%3D
+     
+    try{await ky.post(process.env.REACT_APP_AUDIO, {body: fd}); setIsLoading(false); navigation.navigate('Analyze', {searchVal: value, lang: lang});}
+    catch(error){setIsLoading(false)}
   }
 
-  const langSetting = [
-    {
-      language: 'English',
-      dialects: ['American Dialect', 'Canadian Dialect', 'British Dialect', 'Australia Dialect', 'Indian Dialect'],
-      regionCode: ['en-US', 'en-CA', 'en-GB', 'en-AU', 'en-IN'] 
-    },
-    {language: 'French', dialects: ['French Dialect', 'Canadian Dialect'], regionCode: ['fr-FR', 'fr-CA']},
-    {language: 'Spanish', dialects: ['Spaniard Dialect', 'Mexico Dialect'], regionCode: ['es-ES', 'es-MX']},
-    {language: 'Chinese', dialects: ['Mandarin Dialect (Simplified)', 'Cantonese Dialect (Traditional)'], regionCode: ['zh-CN', 'zh-HK']},
-  ];
+  if (isLoading){
+    return(
+      <ApplicationProvider {...eva} theme = {eva.dark}><Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}><Spinner status= "success" size = "giant" style={{ justifyContent: 'center', alignItems: 'center'}}/></Layout></ApplicationProvider>
+    )
+  }
 
   return (
     <>
@@ -93,7 +98,7 @@ export const Search = ({navigation}) => {
           <Layout>
             <Text style={styles.color = '#f7faff'} category='h2'>Language Assessment</Text>
             <Text style={{textAlign: 'center', marginBottom: '5%'}}>Enter a word or sentence that you would like to pronunce then click the mic and pronunce it!</Text>
-            <Divider style = {styles.test}/>
+            <Divider/>
           </Layout>
           <Layout style={styles.row}>
             <Input style ={styles.input} placeholder = 'Pronunced Word' value={value} accessoryRight={recording ? micFill : micOutline} onChangeText={nextValue => setValue(nextValue)}/> 
@@ -112,7 +117,7 @@ export const Search = ({navigation}) => {
               </SelectGroup>
             ))}
           </Select>
-          <Button style={{marginTop: 5}} status='success' disabled = {value && done && selectedValue ? false : true} appearance='outline' onPress={test}>Analyze your pronunciation!</Button>
+          <Button style={{marginTop: 5}} status='success' disabled = {value && done && selectedValue ? false : true} appearance='outline' onPress={submit}>Analyze your pronunciation!</Button>
         </Layout>
       </ApplicationProvider>    
     </>
