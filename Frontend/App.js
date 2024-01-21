7cf761281b2793d19da90de870019679c6b562b3
ᴸᴿKyle <lr.zkkat@gmail.com>
2024-01-21 18:18:21 -0500

    Configured the UI
    I'm not sure about the current UI, I will consider reconfiguring it.

--- Frontend/App.js
+++ Frontend/App.js
@@ -8,9 +8,9 @@ const Stack = createNativeStackNavigator();
 export default function App() {  
   return(
     <NavigationContainer> 
-      <Stack.Navigator initialRouteName='Analyze'>
+      <Stack.Navigator initialRouteName='Search'>
         <Stack.Screen name="Search" component={Search} options={{title: "", headerStyle: {backgroundColor: '#1b2137'}, headerTitleStyle: {color: 'white'}, animation: 'slide_from_left'}}/>
-        <Stack.Screen name="Analyze" component={Analyze}  initialParams={{searchVal: "Placeholder", langVal: "Placeholder"}} options={{animation: 'none'}}/>
+        <Stack.Screen name="Analyze" component={Analyze}  initialParams={{searchVal: "Placeholder", langVal: "Placeholder"}} options={{headerStyle: {backgroundColor: '#1b2137'}, headerTitleStyle: {color: 'white'}, animation: 'none'}}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
--- Frontend/screens/Analyze.js
+++ Frontend/screens/Analyze.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect}from 'react'
 import * as eva from '@eva-design/eva'
 import {StyleSheet, View} from 'react-native'
-import {ApplicationProvider, Layout, Text} from '@ui-kitten/components'
+import {ApplicationProvider, Card, Divider, Layout, Text} from '@ui-kitten/components'
 import axios from 'axios';
 import {VictoryPie, VictoryAnimation, VictoryLabel} from "victory-native";
 
@@ -27,8 +27,8 @@ export const Analyze = ({route}) => {
   }, []);
 
   const pronunciationScore = [
-    { x: 0, y: Math.round(backData['Pronunciation Score'])},//0
-    { x: 1, y: 100 - Math.round(backData['Pronunciation Score'])},//1
+    { x: 1, y: Math.round(backData['Pronunciation Score'])},//0
+    { x: 2, y: 100 - Math.round(backData['Pronunciation Score'])},//1
   ]
   const completenessScore = [
     { x: 1, y: Math.round(backData['Completeness Score'])},
@@ -43,62 +43,83 @@ export const Analyze = ({route}) => {
     { x: 2, y: 100 - Math.round(backData['Prosody Score'])},
   ];
 
+  const headerPS = (props) => (<Text {...props} style={{backgroundColor: '#0E6655', textAlign:'center'}}>{backData['Pronunciation Score']}</Text>);
+  const headerCS = (props) => (<Text {...props} style={{backgroundColor: '#167d61', textAlign:'center'}}>{backData['Completeness Score']}</Text>);
+  const headerFS = (props) => (<Text {...props} style={{backgroundColor: '#16A085', textAlign:'center'}}>{backData['Fluency Score']}</Text>);
+  const headerProS= (props) => (<Text {...props} style={{backgroundColor: '#08b683', textAlign:'center'}}>{backData['Prosody Score']}</Text>);
+  
+  
   return (
     <>
       <ApplicationProvider {...eva} theme={eva.dark}>
         <Layout style={styles.container}>
-          <Layout style={{position: 'absolute', backgroundColor: 'rgba(0, 0, 0, 0)'}}>
-            <VictoryPie
-              data={pronunciationScore}
-              animate={{ duration: 1000 }}
-              innerRadius={90}
-              cornerRadius={3}
-              colorScale={['red', '#ecf0f1']}
-              width={300}
-              height={300}
-              padding={30}
-            />
-          </Layout>  
-          <Layout style={{position: 'absolute', backgroundColor: 'rgba(0, 0, 0, 0)'}}>
-            <VictoryPie
-              data={completenessScore}
-              animate={{ duration: 1000 }}
-              innerRadius={80}
-              cornerRadius={2}
-              colorScale={['blue', '#ecf0f1']}
-              width={260}
-              height={260}
-              padding={30}
-            />
+            <Layout style={{position: 'absolute', backgroundColor: 'rgba(0, 0, 0, 0)', }}>
+              <VictoryPie
+                data={pronunciationScore}
+                VictoryAnimation={VictoryAnimation}
+                innerRadius={90}
+                cornerRadius={3}
+                colorScale={['#0E6655', '#ecf0f1']}
+                width={300}
+                height={300}
+                padding={30}
+                labels={() => null}
+              />
+            </Layout>  
+            <Layout style={{position: 'absolute',  backgroundColor: 'rgba(0, 0, 0, 0)', borderWidth: 20, borderColor: 'rgba(0, 0, 0, 0)'}}>
+              <VictoryPie
+                data={completenessScore}
+                innerRadius={80}
+                cornerRadius={2}
+                colorScale={['#167d61', '#ecf0f1']}
+                width={260}
+                height={260}
+                padding={30}
+                labels={() => null}
+              />
+            </Layout>
+            <Layout style={{position: 'absolute', backgroundColor: 'rgba(0, 0, 0, 0)', borderWidth: 40, borderColor: 'rgba(0, 0, 0, 0)'}}>
+              <VictoryPie
+                data={fluencyScore}
+                innerRadius={60}
+                cornerRadius={2}
+                colorScale={['#16A085', '#ecf0f1']}
+                width={220}
+                height={220}
+                padding={30}
+                labels={() => null}
+              />
+            </Layout>
+            <Layout style={{position: 'absolute', backgroundColor: 'rgba(0, 0, 0, 0)', borderWidth: 75, borderColor: 'rgba(0, 0, 0, 0)'}}>
+              <VictoryPie
+                data={prosodyScore}
+                innerRadius={60}
+                cornerRadius={2}
+                colorScale={['#08b683', '#ecf0f1']}
+                width={150}
+                height={150}
+                padding={30}
+                labels={() => null}
+              />
+            </Layout>
+            <Layout style={{position: 'absolute', backgroundColor: 'rgba(0, 0, 0, 0)', borderWidth: 125, borderColor: 'rgba(0, 0, 0, 0)'}}>
+              <Text category='h1'>{backData['Overall Accuracy Score']}</Text>
+            </Layout>
+            
+            <Layout style={[styles.resultKeys, {marginTop: '75%'}]}>
+              <Card style={styles.card} header={headerPS}><Text style={{textAlign:'center'}}>Pronunciation Score</Text></Card>
+              <Card style={styles.card} header={headerCS}><Text style={{textAlign:'center'}}>Completeness Score</Text></Card>
+            </Layout>
+            <Layout style={[styles.resultKeys, {marginTop: '100%'}]}>
+              <Card style={styles.card} header={headerFS}><Text style={{textAlign:'center'}}>Fluency {"\n"} Score</Text></Card>
+              <Card style={styles.card} header={headerProS}><Text style={{textAlign:'center'}}>Prosody {"\n"} Score</Text></Card>
+            </Layout>
+            <Layout style={[styles.resultKeys, {marginTop: '75%'}]}>
+            
+            </Layout>
+
           </Layout>
-          <Layout style={{position: 'absolute', backgroundColor: 'rgba(0, 0, 0, 0)'}}>
-            <VictoryPie
-              data={fluencyScore}
-              animate={{ duration: 1000 }}
-              innerRadius={60}
-              cornerRadius={2}
-              colorScale={['yellow', '#ecf0f1']}
-              width={220}
-              height={220}
-              padding={30}
-            />
-          </Layout>
-          <Layout style={{position: 'absolute', backgroundColor: 'rgba(0, 0, 0, 0)'}}>
-            <VictoryPie
-              data={prosodyScore}
-              animate={{ duration: 1000 }}
-              innerRadius={60}
-              cornerRadius={2}
-              colorScale={['green', '#ecf0f1']}
-              width={150}
-              height={150}
-              padding={30}
-            />
-          </Layout>
-          <Layout style={{position: 'absolute', backgroundColor: 'rgba(0, 0, 0, 0)'}}>
-            <Text category='h1'>{backData['Overall Accuracy Score']}</Text>
-          </Layout>
-        </Layout>
+          
       </ApplicationProvider>
     </>
   );
@@ -108,8 +129,23 @@ const styles = StyleSheet.create({
     container: {
       flex: 1,
       alignItems: 'center',
-      justifyContent: 'center',
+    },
+    resultKeys: {
+      flex: 1, 
+      alignItems: 'center',
+      justifyContent:'center', 
+      backgroundColor: 'rgba(0, 0, 0, 0)', 
+      flexDirection:'row',
+      position: 'absolute',
+    },
+    card: {
+      margin: 2,
+      width: 150,
+      height: 90,
+      backgroundColor: '#1b2137'
     },
 });
 //'#4658db'
 //backData["Overall Accuracy Score"]
+//Instead of borderWidth, you could have used MarginTop and MarginLeft to position the progress bars
+//const headerOAS= (props) => (<Text {...props} style={{backgroundColor: 'white', textAlign:'center', color: 'black'}}>{backData['Overall Accuracy Score']}</Text>);
